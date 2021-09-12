3da6052924f3f224b3acd8b2d96b94f042a5cdbd
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2021-09-11 22:10:09 -0700

    rendered initial api calls for homepage

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 const Card = ({ name, weather, icon, temp, tempMax, tempMin }) => {
+
     return (
-        <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg bg-gray-900 dark:bg-gray-50">
+        <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg bg-gray-900 dark:bg-gray-50 transition duration-500">
             <div className="px-4 py-2">
-                <h1 className="text-3xl font-bold text-gray-50 uppercase dark:text-gray-900">{name}</h1>
-                <p className="mt-1 text-sm text-gray-50 dark:text-gray-900">{weather}</p>
-                <p>{icon}</p>
-                <p className='text-gray-50 dark:text-gray-900'>Current {Math.round(temp)}{'\u00B0'}</p>
-                <p className='text-gray-50 dark:text-gray-900'>High {Math.round(tempMax)}{'\u00B0'}</p>
-                <p className='text-gray-50 dark:text-gray-900'>Low {Math.round(tempMin)}{'\u00B0'}</p>
+                <h1 className="text-3xl font-bold text-gray-50 uppercase dark:text-gray-900 transition duration-500">{name}</h1>
+                <p className="mt-1 text-sm text-gray-50 dark:text-gray-900 transition duration-500">{weather}</p>
+                <p className='text-gray-50 dark:text-gray-900 transition duration-500'>{icon}</p>
+                <p className='text-gray-50 dark:text-gray-900 transition duration-500'>Current {Math.round(temp)}{'\u00B0'}</p>
+                <p className='text-gray-50 dark:text-gray-900 transition duration-500'>High {Math.round(tempMax)}{'\u00B0'}</p>
+                <p className='text-gray-50 dark:text-gray-900 transition duration-500'>Low {Math.round(tempMin)}{'\u00B0'}</p>
             </div>
         </div>
     )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,45 +1,62 @@
 import React, { useState, useEffect } from 'react'
 import Button from '../components/Button'
 import Spinner from '../components/Spinner'
-import useDarkMode from '../hook/useDarkMode'
 import Card from '../components/Card'
 import { UilCloudSun } from '@iconscout/react-unicons'
+import { UilThunderstormSun } from '@iconscout/react-unicons'
+import { UilSun } from '@iconscout/react-unicons'
 
 const HomePage = () => {
-    // useDarkMode();
     const API_KEY = '43809593cd4fd991205d21be2120e27e'
-    const [city, setCity] = useState('new york')
+    const [cityOne, setCityOne] = useState('new york')
+    const [cityTwo, setCityTwo] = useState('san diego')
+    const [cityThree, setCityThree] = useState('paris')
+    const [cityFour, setCityFour] = useState('tokyo')
+    const [cityFive, setCityFive] = useState('dubai')
     const [data, setData] = useState({})
     const [isLoading, setIsLoading] = useState(true)
+    const [icon, setIcon] = useState('icon')
 
     useEffect(() => {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial&cnt=40`)
-            .then(response => response.json())
-            .then(res => {
-                setData(res)
+            Promise.all([
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityOne}&appid=${API_KEY}&units=imperial&cnt=40`),
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityTwo}&appid=${API_KEY}&units=imperial&cnt=40`),
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityThree}&appid=${API_KEY}&units=imperial&cnt=40`),
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityFour}&appid=${API_KEY}&units=imperial&cnt=40`),
+                fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityFive}&appid=${API_KEY}&units=imperial&cnt=40`)
+            ]).then(responses => {
+                return Promise.all(responses.map(res => {
+                    return res.json()
+                }))
+            }).then(data => {
+                setData(data)
                 setIsLoading(false)
-                console.log(res)
-            })
-    }, [city])
+                console.log(data)
+            }).catch(error => console.log(error))
+    }, [cityOne, cityTwo])
 
     
     return (
         <div>
             {isLoading ? <Spinner /> : (
                 <div className='h-screen flex flex-col justify-center items-center space-y-10'>
-                    <h1 className='text-5xl font-bold'>welcome to coolWeather</h1>
+                    <h1 className='text-5xl font-bold'>Welcome to coolWeather</h1>
                     <Button className='font-bold uppercase self-center text-xl sm:text-2xl w-60 text-center bg-indigo-500 shadow-2xl p-6 rounded-full text-gray-50'>
                         explore
                     </Button>
-                    <Card 
-                        name={data.name}
-                        weather={data.weather[0].main}
-                        icon={<UilCloudSun color='#6366f1' />}
-                        temp={data.main.temp}
-                        tempMax={data.main.temp_max}
-                        tempMin={data.main.temp_min}
-                    >
-                    </Card>
+                    <div className='flex space-x-10'>
+                        {Object.keys(data).map((city, i) => {
+                        return <Card 
+                                key={i} 
+                                name={data[city].name}
+                                weather={data[city].weather[0].main}
+                                temp={data[city].main.temp}
+                                tempMax={data[city].main.temp_max}
+                                tempMin={data[city].main.temp_min}
+                            >
+                            </Card>
+                        })}
+                    </div>
                 </div>
             )}
         </div>
